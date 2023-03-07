d328448281bdfad6edd877c3dec6b14f2f11f150
ganavibn743 <127034469+ganavibn743@users.noreply.github.com>
2023-03-07 16:28:43 +0530

    Add files via upload

---
+++ assignment1Q.py
@@ -0,0 +1,5 @@
+#Python program to get the Fibonacci series between 0 to 50
+f, s=0, 1
+while s<50:
+    print(s)
+    f, s=s, f+s
---
+++ assignment3Q.py
@@ -0,0 +1,3 @@
+the_input=input("Enter String:")
+reversed=the_input[::-1]
+print(reversed)
